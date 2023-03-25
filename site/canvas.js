6cb1968dad757b1f349a0e030043bf3035d09a92
bodnarbalazs <novroax@gmail.com>
2023-03-25 15:19:30 +0100

    s

--- site/canvas.js
+++ site/canvas.js
@@ -1,14 +1,30 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var width = canvas.width;
-var height = canvas.height;
+const canvas = document.getElementById('canvas');
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(40.7128, -74.0060), // New York coordinates as an example
+        disableDefaultUI: true,
+    };
 
-var svg = new Image();
-svg.src = "assets/europe.svg";
+    const map = new google.maps.Map(canvas, mapOptions);
 
-ctx.fillStyle = "rgb(24, 41, 85)";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const resizeMap = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
 
-svg.onload = function () {
-    ctx.drawImage(svg, 0, 0, width, height);
-};
+        canvas.width = width;
+        canvas.height = height;
+        google.maps.event.trigger(map, 'resize');
+    };
+
+    window.addEventListener('resize', resizeMap);
+    resizeMap();
+
+    google.maps.event.addListener(map, 'click', function (event) {
+        const latitude = event.latLng.lat();
+        const longitude = event.latLng.lng();
+        console.log('Clicked coordinates:', latitude, longitude);
+    });
+});
+
