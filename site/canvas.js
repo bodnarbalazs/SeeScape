49bba1080fa2595d75e8627a559abd560cd215c4
bodnarbalazs <novroax@gmail.com>
2023-03-25 21:09:06 +0100

    s

--- site/canvas.js
+++ site/canvas.js
@@ -1,28 +1,35 @@
 
 function initMap() {
+    loadMapOptions().then((mapOptions) => {
+        const mapElement = document.getElementById('map');
+
+        const map = new google.maps.Map(mapElement, mapOptions);
+
+        const resizeMap = () => {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+
+            mapElement.style.width = `${width}px`;
+            mapElement.style.height = `${height}px`;
+            google.maps.event.trigger(map, 'resize');
+        };
+
+        window.addEventListener('resize', resizeMap);
+        resizeMap();
+
+        google.maps.event.addListener(map, 'click', function (event) {
+            const latitude = event.latLng.lat();
+            const longitude = event.latLng.lng();
+            console.log('Clicked coordinates:', latitude, longitude);
+        });
+    });
+
+
+}
+async function loadMapOptions() {
     const response = await fetch('assets/mapOptions.json');
     const mapOptions = await response.json();
-
-    const mapElement = document.getElementById('map');
-
-    const map = new google.maps.Map(mapElement, mapOptions);
-
-    const resizeMap = () => {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-
-        mapElement.style.width = `${width}px`;
-        mapElement.style.height = `${height}px`;
-        google.maps.event.trigger(map, 'resize');
-    };
-
-    window.addEventListener('resize', resizeMap);
-    resizeMap();
-
-    google.maps.event.addListener(map, 'click', function (event) {
-        const latitude = event.latLng.lat();
-        const longitude = event.latLng.lng();
-        console.log('Clicked coordinates:', latitude, longitude);
-    });
+    return mapOptions;
 }
+
 
