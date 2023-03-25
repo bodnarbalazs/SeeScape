32e8fe01e6221f9452357445999c583a4dbe4ba7
bodnarbalazs <novroax@gmail.com>
2023-03-25 20:57:50 +0100

    s

--- site/canvas.js
+++ site/canvas.js
@@ -35,6 +35,8 @@ function initMap() {
                 }
             },
             "disableDefaultUI": true,
+        },
+        {
             "elementType": "geometry",
             "stylers": [
                 {
