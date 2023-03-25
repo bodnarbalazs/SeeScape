2e4988093e5320fa0031dd66d17a8c69b300d3a6
bodnarbalazs <novroax@gmail.com>
2023-03-25 21:21:35 +0100

    s

--- site/canvas.js
+++ site/canvas.js
@@ -1,35 +1,36 @@
-
 function initMap() {
-    loadMapOptions().then((mapOptions) => {
-        const mapElement = document.getElementById('map');
+    loadMapOptions()
+        .then((data) => {
+            const mapOptions = data.mapOptions;
+            const mapStyles = data.mapStyles;
 
-        const map = new google.maps.Map(mapElement, mapOptions);
+            const mapElement = document.getElementById('map');
+            const map = new google.maps.Map(mapElement, mapOptions);
+            map.setOptions({ styles: mapStyles });
 
-        const resizeMap = () => {
-            const width = window.innerWidth;
-            const height = window.innerHeight;
+            const resizeMap = () => {
+                const width = window.innerWidth;
+                const height = window.innerHeight;
 
-            mapElement.style.width = `${width}px`;
-            mapElement.style.height = `${height}px`;
-            google.maps.event.trigger(map, 'resize');
-        };
+                mapElement.style.width = `${width}px`;
+                mapElement.style.height = `${height}px`;
+                google.maps.event.trigger(map, 'resize');
+            };
 
-        window.addEventListener('resize', resizeMap);
-        resizeMap();
+            window.addEventListener('resize', resizeMap);
+            resizeMap();
 
-        google.maps.event.addListener(map, 'click', function (event) {
-            const latitude = event.latLng.lat();
-            const longitude = event.latLng.lng();
-            console.log('Clicked coordinates:', latitude, longitude);
-        });
-    });
-
-
+            google.maps.event.addListener(map, 'click', function (event) {
+                const latitude = event.latLng.lat();
+                const longitude = event.latLng.lng();
+                console.log('Clicked coordinates:', latitude, longitude);
+            });
+        })
+        .catch((error) => console.error('Error fetching map options:', error));
 }
+
 async function loadMapOptions() {
     const response = await fetch('assets/mapOptions.json');
-    const mapOptions = await response.json();
-    return mapOptions;
+    const data = await response.json();
+    return data;
 }
-
-
