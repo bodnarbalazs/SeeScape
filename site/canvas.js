4a1f6ffeee7ec04fef672f2f9fd860e4058838cc
bodnarbalazs <novroax@gmail.com>
2023-03-25 15:09:03 +0100

    s

--- site/canvas.js
+++ site/canvas.js
@@ -3,8 +3,12 @@ var ctx = canvas.getContext("2d");
 var width = canvas.width;
 var height = canvas.height;
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, width, height);
-ctx.fillStyle = "white";
-ctx.font = "10px Arial";
-ctx.fillText("Hello, world!", 10, 10);
+var svg = new Image();
+svg.src = "assets/europe.svg";
+
+ctx.fillStyle = "rgb(24, 41, 85)";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+svg.onload = function () {
+    ctx.drawImage(svg, 0, 0, width, height);
+};
