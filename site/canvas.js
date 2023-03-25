d9ca38bbb004dcb6b5da72eeb1959e3e13a04c14
bodnarbalazs <novroax@gmail.com>
2023-03-25 15:00:01 +0100

    s

--- site/canvas.js
+++ site/canvas.js
@@ -4,5 +4,5 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = "red";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = "white";
-ctx.font = "30px Arial";
+ctx.font = "100px Arial";
 ctx.fillText("Hello, world!", 10, 50);
