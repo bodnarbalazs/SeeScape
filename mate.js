9986cc48916648cbd9aa26360b2cca8106b20910
Bene Máté <bene.mate2002@gmail.com>
2023-03-26 00:41:42 +0100

    asdsad

--- mate.js
+++ mate.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var startX = 600;
+var startX = 1200;
 var startY = 300;
-var endX = 700;
+var endX = 500;
 var endY = 200;
 
 var x = startX;
@@ -22,20 +22,16 @@ ctx.strokeStyle = '#adfc03';
 ctx.lineWidth = 2;
 
 function animate() {
-  // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw the line
   ctx.beginPath();
   ctx.moveTo(startX, startY);
   ctx.lineTo(x, y);
   ctx.stroke();
 
-  // Update the starting point of the line
   x += DiffX;
   y += DiffY;
 
-  // Stop the animation when the line reaches the endpoint
   if ((DiffX > 0 && x > endX) || (DiffX < 0 && x < endX) || (DiffY > 0 && y > endY) || (DiffY < 0 && y < endY)) {
     console.log("Vége öreg");
     cancelAnimationFrame(animationId);
