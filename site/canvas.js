0afac9502ca8e524a3f7d3daf42db54bcbde2acf
VSZA <vszandras@gmail.com>
2023-03-26 04:37:59 +0200

    asd

--- site/canvas.js
+++ site/canvas.js
@@ -187,9 +187,9 @@ function place_loc(longitude, latitude, x, y){
             Boatlongitude = longitude;
             Boatlatitude = latitude;
             
-            var distance = distance(Baselongitude,Baselatitude,Boatlongitude,Boatlatitude)
+            var distance = distance(Baselongitude,Baselatitude,Boatlongitude,Boatlatitude);
             document.getElementById("instruction").innerHTML = "The distance between the boat and the base: " + distance;
-            count = 2;
+            count += 2;
             console.log("count" + count);
         }
     }
