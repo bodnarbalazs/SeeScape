29a22094afd2c630f8d87758a2749f48907c0537
VSZA <vszandras@gmail.com>
2023-03-26 06:11:56 +0200

    asd

--- site/canvas.js
+++ site/canvas.js
@@ -125,7 +125,7 @@ if (startY > endY) {
 ctx.strokeStyle = '#fffff';
 ctx.lineWidth = 3;
 
-function animate(startX,startY,x,y) {
+function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.beginPath();
@@ -197,7 +197,49 @@ function place_loc(longitude, latitude, x, y){
             count += 2;
             console.log("count" + count);
 
-            animate(basex,basey,x,y)
+            // dotted line
+            var startX = basex;
+            var startY = basey;
+            var endX = x;
+            var endY = y;
+
+            var x = startX;
+            var y = startY;
+            var DiffX = Math.abs(endX - startX) / 100;
+            var DiffY = Math.abs(endY - startY) / 100;
+
+            if (startX > endX) {
+            DiffX = -DiffX;
+            }
+            if (startY > endY) {
+            DiffY = -DiffY;
+            }
+
+            ctx.strokeStyle = '#fffff';
+            ctx.lineWidth = 3;
+
+            function animate() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            ctx.beginPath();
+            ctx.moveTo(startX, startY);
+            ctx.lineTo(x, y);
+            ctx.shadowBlur = 10;
+            ctx.shadowColor = 'white';
+            ctx.strokeStyle = 'white';
+            ctx.stroke();
+
+            x += DiffX;
+            y += DiffY;
+
+            if ((DiffX > 0 && x > endX) || (DiffX < 0 && x < endX) || (DiffY > 0 && y > endY) || (DiffY < 0 && y < endY)) {
+                //console.log("Vége öreg");
+            } else {
+                animationId = requestAnimationFrame(animate);
+            }
+            }
+            var animationId = requestAnimationFrame(animate);
+            // dotted line
 
             document.getElementById("droneTop").style.display = "block";
             document.getElementById("droneTop").classList.add('droneTop_animate');
