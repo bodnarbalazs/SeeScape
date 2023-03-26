3a984adceed75b28dfd5862d58b1692b51b7a1cc
Bene Máté <bene.mate2002@gmail.com>
2023-03-26 01:54:44 +0100

    asd

--- mate.js
+++ mate.js
@@ -37,54 +37,11 @@ function animate() {
 
   if ((DiffX > 0 && x > endX) || (DiffX < 0 && x < endX) || (DiffY > 0 && y > endY) || (DiffY < 0 && y < endY)) {
     //console.log("Vége öreg");
-    animateCircle();
   } else {
     animationId = requestAnimationFrame(animate);
   }
 }
 
-function animateCircle() {
-  // Set the radius of the circle
-  var radius = 50;
 
-  // Set the duration of the animation
-  var animationDuration = 50; // in milliseconds
-
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
- context.lineWidth = 10;
- context.strokeStyle = '#ad2323';
- context.shadowOffsetX = 0;
- context.shadowOffsetY = 0;
- context.shadowBlur = 10;
- context.shadowColor = '#656565';
-
-
-  // Set the global alpha value back to 1
-  ctx.globalAlpha = 1;
-
-  // Draw the circle
-  ctx.beginPath();
-  ctx.arc(endX, endY, radius, Math.PI, 2 * Math.PI);
-  ctx.shadowBlur = 10;
-  ctx.shadowColor = 'white';
-  ctx.strokeStyle = 'white';
-  ctx.stroke();
-
-  // Draw the line
-  ctx.beginPath();
-  ctx.moveTo(startX, startY);
-  ctx.lineTo(endX, endY);
-  ctx.shadowBlur = 10;
-  ctx.shadowColor = 'white';
-  ctx.strokeStyle = 'white';
-  ctx.stroke();
-
-  // Wait for the animation duration to finish before stopping the animation
-  setTimeout(function() {
-    animationId = requestAnimationFrame(animate);
-  }, animationDuration);
-}
 
 var animationId = requestAnimationFrame(animate);
