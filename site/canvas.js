e0d9dafc6f07730d26aaf1ffaeda614680e2d23c
VSZA <vszandras@gmail.com>
2023-03-26 14:40:05 +0200

    as

--- site/canvas.js
+++ site/canvas.js
@@ -395,23 +395,6 @@ function boat2(){
                     activeb = 0;
                 }, 21000);
             }
-            else{
-                setTimeout(function() {
-                    activeb = 1;
-                    document.getElementById("policeBoat").style.display = "block";
-                    document.getElementById("policeBoat").classList.add('policeboat_animate');
-                    document.getElementById("policeBoat").style.top = basey2 + "px";
-                    document.getElementById("policeBoat").style.left = basex2 + "px";
-                    const styleb = document.createElement('style');
-                    console.log(basex2, basey2);
-                    styleb.innerHTML = "@keyframes my-animation2 {0% {top: " + basey2 + "px; left: "+basex2+"px;} 49% {transform: rotate(0deg);} 50% {top: " + y2 + "px; left: "+x2+"px; transform: rotate(180deg);} 100% {top: " + basey2 + "px; left: "+basex2+"px; transform: rotate(180deg);}}";
-                    document.head.appendChild(styleb);
-
-                    setTimeout(function() {
-                        activeb = 0;
-                    }, 21000);
-                }, 13000);
-            }
            
         }, 5000);
 
@@ -461,10 +444,10 @@ function boat3(){
         setTimeout(function() {
             if (activeb == 0){
                 activeb = 1;
-                document.getElementById("policeBoat").style.display = "block";
-                document.getElementById("policeBoat").classList.add('policeboat_animate');
-                document.getElementById("policeBoat").style.top = basey + "px";
-                document.getElementById("policeBoat").style.left = basex + "px";
+                document.getElementById("policeBoat2").style.display = "block";
+                document.getElementById("policeBoat2").classList.add('policeboat_animate');
+                document.getElementById("policeBoat2").style.top = basey + "px";
+                document.getElementById("policeBoat2").style.left = basex + "px";
                 const styleb = document.createElement('style');
                 console.log(basex, basey);
                 styleb.innerHTML = "@keyframes my-animation2 {0% {top: " + basey + "px; left: "+basex+"px;} 49% {transform: rotate(0deg);} 50% {top: " + y + "px; left: "+x+"px; transform: rotate(180deg);} 100% {top: " + basey + "px; left: "+basex+"px; transform: rotate(180deg);}}";
@@ -473,23 +456,6 @@ function boat3(){
                 setTimeout(function() {
                     activeb = 0;
                 }, 17000);
-            }
-            else{
-                setTimeout(function() {
-                    activeb = 1;
-                    document.getElementById("policeBoat").style.display = "block";
-                    document.getElementById("policeBoat").classList.add('policeboat_animate');
-                    document.getElementById("policeBoat").style.top = basey2 + "px";
-                    document.getElementById("policeBoat").style.left = basex2 + "px";
-                    const styleb = document.createElement('style');
-                    console.log(basex2, basey2);
-                    styleb.innerHTML = "@keyframes my-animation2 {0% {top: " + basey2 + "px; left: "+basex2+"px;} 49% {transform: rotate(0deg);} 50% {top: " + y2 + "px; left: "+x2+"px; transform: rotate(180deg);} 100% {top: " + basey2 + "px; left: "+basex2+"px; transform: rotate(180deg);}}";
-                    document.head.appendChild(styleb);
-
-                    setTimeout(function() {
-                        activeb = 0;
-                    }, 17000);
-                }, 9000);
             }
         }, 5000);
 
