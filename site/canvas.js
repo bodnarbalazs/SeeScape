e5f63ab5127a07150786f40bcabcebe3c3369163
VSZA <vszandras@gmail.com>
2023-03-26 04:44:31 +0200

    asd

--- site/canvas.js
+++ site/canvas.js
@@ -196,13 +196,13 @@ function place_loc(longitude, latitude, x, y){
     
 }
 
-function distance(lat1, lon1, lat2, lon2) {
+function distance(Baselatitude, Baselongitude, Boatlatitude, Boatlongitude) {
     const R = 6371; // Radius of the earth in km
-    const dLat = deg2rad(lat2 - lat1);
-    const dLon = deg2rad(lon2 - lon1); 
+    const dLat = deg2rad(Boatlatitude - Baselatitude);
+    const dLon = deg2rad(Boatlongitude - Baselongitude); 
     const a = 
       Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.cos(deg2rad(Baselatitude)) * Math.cos(deg2rad(Boatlatitude)) * 
       Math.sin(dLon/2) * Math.sin(dLon/2); 
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
     const d = R * c; // Distance in km
