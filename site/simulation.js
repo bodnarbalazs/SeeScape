382fd1bccd67e8b2526cd909fbcfe1d8c0cc374c
VSZA <vszandras@gmail.com>
2023-03-26 03:36:48 +0200

    try

--- site/simulation.js
+++ site/simulation.js
@@ -1,3 +1,15 @@
 setTimeout(() => {
     console.log(canvas.width);
 }, 1000);
+
+latitude = canvas.latitude;
+longitude = canvas.longitude;
+setTimeout(() => {
+    console.log("elso" + latitude);
+    console.log("elso long" + longitude);
+}, 2000);
+
+setTimeout(() => {
+    console.log("2" + latitude);
+    console.log("2 long" + longitude);
+}, 4000);
